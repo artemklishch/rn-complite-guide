8bea2b80699a8f8c84cdc0c7fb9d4712ebd1d1f6
artemklishch <artemklishch@gmail.com>
2020-12-08 22:41:46 +0200

    eru

--- App.js
+++ App.js
@@ -3,28 +3,27 @@ import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Cource Goal"
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-            width: "80%",
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Cource Goal" style={styles.textField} />
         <Button title="ADD" />
       </View>
-      <View></View>
+      <View />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: { padding: 50 },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textField: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+});
